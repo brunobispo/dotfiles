0b136e1bddf2fdbebf6ef198df18b5d43dc4d334
Bruno Bispo <bbispo@brex.com>
2024-11-08 19:27:09 -0300

    fix telescope's fzf installation

--- config/nvim/lua/bbispo/plugins/telescope.lua
+++ config/nvim/lua/bbispo/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' }
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
   },
   config = function()
     require('telescope').setup {
