92ff49b70019347989ab53ae7b90a5aab4743d74
Bruno Bispo <bbispo@brex.com>
2024-11-12 10:12:13 -0300

    disable formatting on save

--- config/nvim/lua/bbispo/plugins/formatting.lua
+++ config/nvim/lua/bbispo/plugins/formatting.lua
@@ -36,11 +36,11 @@ return {
         teal = "tl",
         typescript = "ts",
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 500,
-      },
+      -- format_on_save = {
+      --   lsp_fallback = true,
+      --   async = false,
+      --   timeout_ms = 500,
+      -- },
     })
 
     vim.keymap.set({ "n", "v" }, "<leader>ff", function()
