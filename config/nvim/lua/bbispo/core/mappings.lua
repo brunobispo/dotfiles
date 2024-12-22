5e972822a8f27c9890ff8f3160a73d56362902e9
Bruno Bispo <bbispo@brex.com>
2024-12-22 18:40:23 -0300

    add keymap to clean up search highlights

--- config/nvim/lua/bbispo/core/mappings.lua
+++ config/nvim/lua/bbispo/core/mappings.lua
@@ -2,8 +2,11 @@ vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 
 -- copy current path to clipboard
-vim.keymap.set('n', '<leader>cp', ':let @+ = expand("%")<cr>', { noremap = true })
+vim.keymap.set("n", "<leader>cp", ':let @+ = expand("%")<cr>', { noremap = true })
 
 -- remap escape to jk
-vim.keymap.set('i', 'jk', '<esc>', { noremap = true })
-vim.keymap.set('i', '<esc>', '<nop>', { noremap = true })
+vim.keymap.set("i", "jk", "<esc>", { noremap = true })
+vim.keymap.set("i", "<esc>", "<nop>", { noremap = true })
+
+-- clean search highlights
+vim.keymap.set("n", "<leader>/", ":nohlsearch<cr>", { noremap = true })
