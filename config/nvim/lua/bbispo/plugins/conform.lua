5b3be594d8bfc1da3a245232543711d535ffaf18
Bruno Bispo <bbispo@brex.com>
2025-03-18 14:12:31 -0300

    feat: support different formatter for js projects

--- config/nvim/lua/bbispo/plugins/conform.lua
+++ config/nvim/lua/bbispo/plugins/conform.lua
@@ -1,26 +1,48 @@
 return {
 	"stevearc/conform.nvim",
+	dependencies = {
+		{
+			"williamboman/mason.nvim",
+			opts = function(_, opts)
+				opts.ensure_installed = opts.ensure_installed or {}
+				table.insert(opts.ensure_installed, "js-debug-adapter")
+			end,
+		},
+	},
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local conform = require("conform")
 
+		local formatters_by_ft = {
+			javascript = { "prettierd" },
+			typescript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			typescriptreact = { "prettierd" },
+			svg = { "prettierd" },
+			svelte = { "prettierd" },
+			css = { "prettierd" },
+			html = { "prettierd" },
+			json = { "prettierd" },
+			yaml = { "prettierd" },
+			markdown = { "prettierd" },
+			graphql = { "prettierd" },
+			lua = { "stylua" },
+			python = { "isort", "black" },
+		}
+
+		-- use biome if it's in package.json
+		local package_json = vim.fn.getcwd() .. "/package.json"
+		if vim.fn.filereadable(package_json) == 1 then
+			local package = vim.fn.json_decode(vim.fn.readfile(package_json))
+			if package and package.devDependencies and package.devDependencies["@biomejs/biome"] then
+				for _, ft in ipairs({ "javascript", "typescript", "javascriptreact", "typescriptreact" }) do
+					formatters_by_ft[ft] = { "biome" }
+				end
+			end
+		end
+
 		conform.setup({
-			formatters_by_ft = {
-				javascript = { "biome" },
-				typescript = { "biome" },
-				javascriptreact = { "biome" },
-				typescriptreact = { "biome" },
-				svg = { "prettierd" },
-				svelte = { "prettierd" },
-				css = { "prettierd" },
-				html = { "prettierd" },
-				json = { "prettierd" },
-				yaml = { "prettierd" },
-				markdown = { "prettierd" },
-				graphql = { "prettierd" },
-				lua = { "stylua" },
-				python = { "isort", "black" },
-			},
+			formatters_by_ft = formatters_by_ft,
 			format_on_save = {
 				lsp_fallback = true,
 				async = false,
