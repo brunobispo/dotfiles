4d0906bb62ef1e2664a432f82edf8fdae8ddd2e9
Bruno Bispo <bbispo@brex.com>
2025-03-18 14:11:28 -0300

    refactor: group nvim general configs

--- config/nvim/lua/bbispo/core/general.lua
+++ config/nvim/lua/bbispo/core/general.lua
@@ -1,23 +1,37 @@
-vim.opt.clipboard:append("unnamedplus")
+-- general
 vim.opt.encoding = "UTF-8"
 vim.opt.hidden = true
 vim.opt.autochdir = false
 vim.opt.errorbells = false
-vim.opt.signcolumn = "yes"
+vim.opt.number = true
+vim.opt.showmode = false
+vim.opt.cursorline = true
+vim.opt.timeout = false
+vim.opt.mouse = "a"
+vim.opt.exrc = true
+
+-- clipboard
+vim.opt.clipboard:append("unnamedplus")
+
+-- splits
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.undofile = true
+
+-- indentation
 vim.opt.smartindent = true
-vim.opt.spelllang = "en"
 vim.opt.autoindent = true
-vim.opt.number = true
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.tabstop = 2
 vim.opt.expandtab = true
+
+-- folding
+vim.opt.foldcolumn = "0"
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+
+-- others
+vim.opt.signcolumn = "yes"
+vim.opt.undofile = true
+vim.opt.spelllang = "en"
 vim.opt.wrap = false
-vim.opt.timeout = false
-vim.opt.mouse = "a"
-vim.opt.showmode = false
-vim.opt.foldmethod = "marker"
-vim.o.cursorline = true
