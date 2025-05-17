9fb1e6d721b2eec506dddaabdcf23134ea54eea9
Bruno Bispo <me@brunobispo.com>
2025-05-17 20:29:08 -0300

    feat: change theme to catppuccin

--- config/nvim/lua/bbispo/plugins/theme.lua
+++ config/nvim/lua/bbispo/plugins/theme.lua
@@ -1,12 +1,17 @@
 return {
-	"rose-pine/neovim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-		require("rose-pine").setup({
-			variant = "main", -- auto, main, moon, or dawn
-		})
-		-- vim.o.background = "dark"
-		vim.cmd([[colorscheme rose-pine]])
-	end,
+  "catppuccin/nvim",
+  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require("catppuccin").setup({
+      color_overrides = {
+        mocha = {
+          base = "#121212",
+          mantle = "#121212",
+          crust = "#121212",
+        },
+      },
+    })
+    vim.cmd.colorscheme "catppuccin"
+  end,
 }
