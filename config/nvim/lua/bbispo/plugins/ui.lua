0093a2d2a91d3b10aa52ee1d083cb6af96d3314f
Bruno Bispo <me@brunobispo.com>
2025-05-18 18:54:43 -0300

    refactor(nvim): move UI dependencies to dedicated file

---
+++ config/nvim/lua/bbispo/plugins/ui.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "MunifTanjim/nui.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+    event = "VeryLazy",
+  },
+  {
+    "echasnovski/mini.pick",
+    event = "VeryLazy",
+  },
+  {
+    -- support for image pasting
+    "HakonHarnes/img-clip.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- recommended settings
+      default = {
+        embed_image_as_base64 = false,
+        prompt_for_file_name = false,
+        drag_and_drop = {
+          insert_mode = true,
+        },
+        -- required for Windows users
+        use_absolute_path = true,
+      },
+    },
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+      file_types = { "markdown", "Avante" },
+    },
+    ft = { "markdown", "Avante" },
+  },
+}
