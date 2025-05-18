5a33ea0890a532ba73c2b75a99b80532dd8c7798
Bruno Bispo <me@brunobispo.com>
2025-05-18 12:37:49 -0300

    feat: add avante config

--- config/nvim/lua/bbispo/plugins/tree.lua
+++ config/nvim/lua/bbispo/plugins/tree.lua
@@ -1,47 +1,74 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		"antosha417/nvim-lsp-file-operations",
-		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	},
-	config = function()
-		require("neo-tree").setup({
-			filesystem = {
-				window = {
-					mappings = {
-						["o"] = "system_open",
-					},
-				},
-			},
-			commands = {
-				system_open = function(state)
-					local node = state.tree:get_node()
-					local path = node:get_id()
-					-- macOs: open file in default application in the background.
-					vim.fn.jobstart({ "xdg-open", "-g", path }, { detach = true })
-					-- Linux: open file in default application
-					vim.fn.jobstart({ "xdg-open", path }, { detach = true })
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    "antosha417/nvim-lsp-file-operations",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        window = {
+          mappings = {
+            ["o"] = "system_open",
+          },
+        },
+      },
+      commands = {
+        system_open = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          -- macOs: open file in default application in the background.
+          vim.fn.jobstart({ "xdg-open", "-g", path }, { detach = true })
+          -- Linux: open file in default application
+          vim.fn.jobstart({ "xdg-open", path }, { detach = true })
 
-					-- Windows: Without removing the file from the path, it opens in code.exe instead of explorer.exe
-					local p
-					local lastSlashIndex = path:match("^.+()\\[^\\]*$") -- Match the last slash and everything before it
-					if lastSlashIndex then
-						p = path:sub(1, lastSlashIndex - 1) -- Extract substring before the last slash
-					else
-						p = path -- If no slash found, return original path
-					end
-					vim.cmd("silent !start explorer " .. p)
-				end,
-			},
-		})
+          -- Windows: Without removing the file from the path, it opens in code.exe instead of explorer.exe
+          local p
+          local lastSlashIndex = path:match("^.+()\\[^\\]*$") -- Match the last slash and everything before it
+          if lastSlashIndex then
+            p = path:sub(1, lastSlashIndex - 1)               -- Extract substring before the last slash
+          else
+            p = path                                          -- If no slash found, return original path
+          end
+          vim.cmd("silent !start explorer " .. p)
+        end,
 
-		vim.keymap.set("n", "<leader>\\", ":Neotree toggle<cr>", { noremap = true })
-		vim.keymap.set("n", "<leader>|", ":Neotree reveal=true<cr>", { noremap = true })
+        avante_add_files = function(state)
+          local node = state.tree:get_node()
+          local filepath = node:get_id()
+          local relative_path = require('avante.utils').relative_path(filepath)
 
-		require("lsp-file-operations").setup()
-	end,
+          local sidebar = require('avante').get()
+
+          local open = sidebar:is_open()
+          -- ensure avante sidebar is open
+          if not open then
+            require('avante.api').ask()
+            sidebar = require('avante').get()
+          end
+
+          sidebar.file_selector:add_selected_file(relative_path)
+
+          -- remove neo tree buffer
+          if not open then
+            sidebar.file_selector:remove_selected_file('neo-tree filesystem [1]')
+          end
+        end,
+      },
+      window = {
+        mappings = {
+          ['oa'] = 'avante_add_files',
+        },
+      },
+    })
+
+    vim.keymap.set("n", "<leader>\\", ":Neotree toggle<cr>", { noremap = true })
+    vim.keymap.set("n", "<leader>|", ":Neotree reveal=true<cr>", { noremap = true })
+
+    require("lsp-file-operations").setup()
+  end,
 }
