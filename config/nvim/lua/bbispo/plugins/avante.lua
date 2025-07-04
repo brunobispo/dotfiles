da9ef0812e6ce90e418dc8cf34806c2cf365efe4
Bruno Bispo <me@brunobispo.com>
2025-07-04 18:17:06 -0300

    feat: update avante and deps

--- config/nvim/lua/bbispo/plugins/avante.lua
+++ config/nvim/lua/bbispo/plugins/avante.lua
@@ -1,48 +1,50 @@
 return {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  version = false, -- Never set this value to "*"! Never!
-  opts = {
-    provider = "claude",
-    cursor_applying_provider = 'groq',
-    behaviour = {
-      auto_suggestions = false,           -- Experimental stage
-      enable_cursor_planning_mode = true, -- enable cursor planning mode!
-    },
-    openai = {
-      endpoint = "https://api.openai.com/v1",
-      model = "o3",                 -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000,              -- Timeout in milliseconds, increase this for reasoning models
-      temperature = 0,
-      max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-      reasoning_effort = "medium",  -- low|medium|high, only used for reasoning models
-    },
-    groq = {                        -- define groq provider
-      __inherited_from = 'openai',
-      api_key_name = 'GROQ_API_KEY',
-      endpoint = 'https://api.groq.com/openai/v1/',
-      model = 'llama-3.3-70b-versatile',
-      max_completion_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
-    },
-    claude = {
-      endpoint = "https://api.anthropic.com",
-      model = "claude-3-7-sonnet-latest",
-      temperature = 0,
-      max_tokens = 8192,
-    },
-    ollama = {
-      endpoint = "http://127.0.0.1:11434",
-      model = "deepseek-coder:1.3b"
-    },
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-    "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-  },
+	"yetone/avante.nvim",
+	enabled = false, -- Plugin is now disabled
+	event = "VeryLazy",
+	version = false, -- Never set this value to "*"! Never!
+	opts = {
+		provider = "claude",
+		cursor_applying_provider = "groq",
+		mode = "legacy",
+		behaviour = {
+			auto_suggestions = false, -- Experimental stage
+			enable_cursor_planning_mode = true, -- enable cursor planning mode!
+		},
+		openai = {
+			endpoint = "https://api.openai.com/v1",
+			model = "o4-mini", -- your desired model (or use gpt-4o, etc.)
+			timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+			temperature = 0,
+			max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+			reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+		},
+		groq = { -- define groq provider
+			__inherited_from = "openai",
+			api_key_name = "GROQ_API_KEY",
+			endpoint = "https://api.groq.com/openai/v1/",
+			model = "llama-3.3-70b-versatile",
+			max_completion_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
+		},
+		claude = {
+			endpoint = "https://api.anthropic.com",
+			model = "claude-3-7-sonnet-latest",
+			temperature = 0,
+			max_tokens = 8192,
+		},
+		ollama = {
+			endpoint = "http://127.0.0.1:11434",
+			model = "deepseek-coder:1.3b",
+		},
+	},
+	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	build = "make",
+	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+		"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+		"ibhagwan/fzf-lua", -- for file_selector provider fzf
+	},
 }
--- config/nvim/lua/bbispo/plugins/ui.lua
+++ config/nvim/lua/bbispo/plugins/ui.lua
@@ -1,42 +1,27 @@
 return {
-  {
-    "stevearc/dressing.nvim",
-    event = "VeryLazy",
-  },
-  {
-    "MunifTanjim/nui.nvim",
-    event = "VeryLazy",
-  },
-  {
-    "nvim-tree/nvim-web-devicons",
-    event = "VeryLazy",
-  },
-  {
-    "echasnovski/mini.pick",
-    event = "VeryLazy",
-  },
-  {
-    -- support for image pasting
-    "HakonHarnes/img-clip.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- recommended settings
-      default = {
-        embed_image_as_base64 = false,
-        prompt_for_file_name = false,
-        drag_and_drop = {
-          insert_mode = true,
-        },
-        -- required for Windows users
-        use_absolute_path = true,
-      },
-    },
-  },
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    opts = {
-      file_types = { "markdown", "Avante" },
-    },
-    ft = { "markdown", "Avante" },
-  },
+	{ "stevearc/dressing.nvim" },
+	{ "MunifTanjim/nui.nvim" },
+	{ "nvim-tree/nvim-web-devicons" },
+	{ "echasnovski/mini.pick" },
+	{
+		-- support for image pasting
+		"HakonHarnes/img-clip.nvim",
+		opts = {
+			-- recommended settings
+			default = {
+				embed_image_as_base64 = false,
+				prompt_for_file_name = false,
+				drag_and_drop = {
+					insert_mode = true,
+				},
+				-- required for Windows users
+				use_absolute_path = true,
+			},
+		},
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = { file_types = { "markdown", "Avante" } },
+		ft = { "markdown", "Avante" },
+	},
 }
