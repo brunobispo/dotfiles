b409b4b39a672afd9b32d8dcbb93a35ef7f943b0
Bruno Bispo <bbispo@brex.com>
2025-07-09 12:47:03 -0300

    feat: enable avante

--- config/nvim/lua/bbispo/plugins/avante.lua
+++ config/nvim/lua/bbispo/plugins/avante.lua
@@ -1,6 +1,5 @@
 return {
 	"yetone/avante.nvim",
-	enabled = false, -- Plugin is now disabled
 	event = "VeryLazy",
 	version = false, -- Never set this value to "*"! Never!
 	opts = {
@@ -11,30 +10,38 @@ return {
 			auto_suggestions = false, -- Experimental stage
 			enable_cursor_planning_mode = true, -- enable cursor planning mode!
 		},
-		openai = {
-			endpoint = "https://api.openai.com/v1",
-			model = "o4-mini", -- your desired model (or use gpt-4o, etc.)
-			timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-			temperature = 0,
-			max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-			reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-		},
-		groq = { -- define groq provider
-			__inherited_from = "openai",
-			api_key_name = "GROQ_API_KEY",
-			endpoint = "https://api.groq.com/openai/v1/",
-			model = "llama-3.3-70b-versatile",
-			max_completion_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
-		},
-		claude = {
-			endpoint = "https://api.anthropic.com",
-			model = "claude-3-7-sonnet-latest",
-			temperature = 0,
-			max_tokens = 8192,
-		},
-		ollama = {
-			endpoint = "http://127.0.0.1:11434",
-			model = "deepseek-coder:1.3b",
+		providers = {
+			openai = {
+				endpoint = "https://api.openai.com/v1",
+				model = "o4-mini", -- your desired model (or use gpt-4o, etc.)
+				extra_request_body = {
+					timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+					temperature = 0,
+					max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+					reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+				},
+			},
+			groq = { -- define groq provider
+				__inherited_from = "openai",
+				api_key_name = "GROQ_API_KEY",
+				endpoint = "https://api.groq.com/openai/v1/",
+				model = "llama-3.3-70b-versatile",
+				extra_request_body = {
+					max_completion_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
+				},
+			},
+			claude = {
+				endpoint = "https://api.anthropic.com",
+				model = "claude-3-7-sonnet-latest",
+				extra_request_body = {
+					temperature = 0,
+					max_tokens = 8192,
+				},
+			},
+			ollama = {
+				endpoint = "http://127.0.0.1:11434",
+				model = "deepseek-coder:1.3b",
+			},
 		},
 	},
 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
